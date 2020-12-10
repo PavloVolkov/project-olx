9c6542d08c9801d684a15e851a57ff62aa24b3f4
Alexey Golodenko <alexey.golodenko@gmail.com>
2020-12-10 20:50:55 +0200

    добавляет разметку

--- src/js/componentsHTML.js
+++ src/js/componentsHTML.js
@@ -1,8 +1,8 @@
 import { Modal } from 'bootstrap';
-
+const header = require('../partials/header.handlebars')
 const addModal = require('../partials/add-modal.handlebars');
 const authModal = require('../partials/auth-modal.handlebars');
 const myCalls = require('../partials/my-calls.handlebars');
 const footer = require('../partials/footer.handlebars');
 
-export default [authModal, addModal, myCalls, footer]
+export default [header,authModal, addModal, myCalls, footer]
