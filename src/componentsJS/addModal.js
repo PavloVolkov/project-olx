e171f0907cd29a3f058ab26646bad459c6e3bbb6
Maxim-Ibraiev <64267252+Maxim-Ibraiev@users.noreply.github.com>
2020-12-14 22:19:17 +0200

    Revert "Revert "Revert "Merge branch 'Dev' into FM-1/HEADER/styles"""
    This reverts commit 24e8eb9b5dd8a55392943f94c75c855cbe2eb550.

--- src/componentsJS/addModal.js
+++ src/componentsJS/addModal.js
@@ -1,115 +1,13 @@
-const refs = {
-  form: document.querySelector('.js-add-form'),
-  openAddModalBtn: document.querySelector('[data-action="open-modal"]'),
-  closeAddModalBtn: document.querySelector('[data-action="close-modal"]'),
-  backdrop: document.querySelector('.js-backdrop'),
-  selectorCategory: document.querySelector('.add-modal__product-select'),
-  imgLoaderArea: document.querySelector('.add-modal__product-photos'),
-  addImage: document.querySelector('.add-image'),
-};
+// const refs = {
+//   openModalBtn: document.querySelector('.header__create-add-btn'),
+//   closeModalBtn: document.querySelector('.add-modal__close-btn'),
+//   modal: document.querySelector('[data-backdrop]'),
+// };
 
-// Логика закрытия модалки по кнопке закрыть, искейпу и бэкдропу
-refs.openAddModalBtn.addEventListener('click', onOpenAddModal);
-refs.closeAddModalBtn.addEventListener('click', onCloseAddModal);
-refs.backdrop.addEventListener('click', onBackdropClick);
+// refs.openModalBtn.addEventListener('click', toggleModal);
+// refs.closeModalBtn.addEventListener('click', toggleModal);
 
-function onOpenAddModal() {
-  window.addEventListener('keydown', onEscKeyPress);
-  document.body.classList.add('show-modal');
-}
-
-function onCloseAddModal() {
-  window.removeEventListener('keydown', onEscKeyPress);
-  document.body.classList.remove('show-modal');
-}
-
-function onBackdropClick(event) {
-  if (event.currentTarget === event.target) {
-    onCloseAddModal();
-  }
-}
-
-function onEscKeyPress(event) {
-  if (event.code === 'Escape') {
-    onCloseAddModal();
-  }
-}
-
-// Логика добавления изображения и перемещения "+" на следующий блок
-let imgLoaderArea;
-
-refs.imgLoaderArea.addEventListener('click', chooseImgBlock);
-refs.imgLoaderArea.addEventListener('change', previewImg);
-
-//==================================
-function chooseImgBlock(event) {
-  if (event.target === event.currentTarget) {
-    return;
-  }
-  if (!event.target.dataset.active) {
-    return;
-  }
-  const imgTarget = event.target;
-  imgTarget.setAttribute('type', 'file');
-}
-
-//=========================
-function previewImg(event) {
-  if (event.target === event.currentTarget) {
-    return;
-  }
-  changeImgBlock(event);
-  if (event.target.dataset.id) {
-    const file = event.target.files[0];
-    const inputID = event.target.dataset.id;
-    const img = document.querySelector(`.input-label__img--${inputID}`);
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      img.src = reader.result;
-      productImage = reader.result;
-      img.setAttribute('data-img', productImage);
-    };
-    if (file) {
-      reader.readAsDataURL(file);
-    } else {
-      img.src = '';
-    }
-  }
-}
-
-//======================
-function changeImgBlock(event) {
-  const imgTarget = event.target;
-  imgTarget.nextElementSibling.classList.remove('choose-this');
-  let imgId = Number(event.target.dataset.id);
-  imgId += 1;
-  if (imgId > 5) {
-    return;
-  }
-  const nextImg = document.querySelector(`[data-id="${imgId}"]`);
-  nextImg.dataset.active = true;
-  nextImg.nextElementSibling.classList.add('choose-this');
-}
-
-//===========================
-// Логика подгрузки и отображения категорий с бэкенда при нажатии на выпадашку выбора категории
-fetch('https://callboard-backend.herokuapp.com/call/categories')
-  .then(response => response.json())
-  .then(result => {
-    let translationOfWords = {
-      property: 'Нерухомість',
-      transport: ' Транспорт',
-      work: 'Робота',
-      electronics: 'Електроніка',
-      'business and services': 'Бізнес та послуги',
-      'recreation and sport': 'Відпочинок та спорт',
-      free: 'Віддам безкоштовно',
-      trade: 'Обмін',
-    };
-    const markup = result.map(
-      category =>
-        `<option value="${category}" class="select-option">${translationOfWords[category]}</option> `,
-    );
-    refs.selectorCategory.insertAdjacentHTML('beforeend', markup);
-  })
-  .catch(error => console.log('error', error));
+// function toggleModal() {
+//   document.body.classList.toggle('modal-open');
+//   refs.modal.classList.toggle('is-hidden');
+// }
