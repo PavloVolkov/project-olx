27ca4aa734cd04a740156049db99bce4dc839582
Maxim-Ibraiev <64267252+Maxim-Ibraiev@users.noreply.github.com>
2020-12-14 21:51:44 +0200

    Revert "Merge branch 'Dev' into FM-1/HEADER/styles"
    This reverts commit 2d0fc0924bbc932ff2176284e7cee849e17aceae, reversing
    changes made to 9686debdf60f4dcf033602748bd374cb3404c1bf.

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,7 @@
-const refs = {
-  body: document.querySelector('body')
-}
+import refs from './renderComponentsHTML'
+
+refs.myCallSavaBtn = document.querySelector('[data-my-call="save"')
+refs.myCallModal = document.querySelector('.js-my-call-modal')
+
 
 export default refs;
